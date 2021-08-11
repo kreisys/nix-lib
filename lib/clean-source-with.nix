7034b7bf0a0cf9087cf4f9c588dcb46470dd3673
Shay Bergmann <shay.bergmann@iohk.io>
2021-08-11 00:57:54 +0000

    wip

--- lib/clean-source-with.nix
+++ lib/clean-source-with.nix
@@ -1,7 +1,10 @@
 { lib, nix-gitignore }:
 
 let
-  inherit (import (builtins.fetchurl "https://raw.githubusercontent.com/input-output-hk/haskell.nix/09526c85558417de609ced98460392460b025b98/lib/clean-source-with.nix") { inherit lib; }) cleanSourceWith;
+  inherit (import (builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/input-output-hk/haskell.nix/09526c85558417de609ced98460392460b025b98/lib/clean-source-with.nix";
+    sha256 = "1ad16p9dlmwr8bkiz59sd70f5x33b5zcka2svfwp0c87pwv0flzw";
+  }) { inherit lib; }) cleanSourceWith;
   inherit (nix-gitignore) gitignoreFilterPure;
 in
 
