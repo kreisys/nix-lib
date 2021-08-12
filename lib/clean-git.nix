e62c580ac0d2e8133bb2f3326c312ac86d348277
Shay Bergmann <shay.bergmann@iohk.io>
2021-08-12 17:07:48 +0000

    wip

--- lib/clean-git.nix
+++ lib/clean-git.nix
@@ -1,5 +1,8 @@
 { lib, runCommand, git, cleanSourceWith }:
 
-import (builtins.fetchurl "https://raw.githubusercontent.com/input-output-hk/haskell.nix/39914630438b7ee3ff244f39f6cf9bdbc337557f/lib/clean-git.nix") {
+import (builtins.fetchurl {
+    url = "https://raw.githubusercontent.com/input-output-hk/haskell.nix/39914630438b7ee3ff244f39f6cf9bdbc337557f/lib/clean-git.nix";
+    sha256 = "096m08a4isk4fqc2cpz3bh3hg38765gqwfb9z57gwwxsdihxl6j1";
+}) {
     inherit lib runCommand git cleanSourceWith;
   }
